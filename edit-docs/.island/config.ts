188251e226da675d9561e2a33a058e367837923c
cmtlyt <cmtlyt@163.com>
2024-01-17 15:02:54 +0800

    init

---
+++ edit-docs/.island/config.ts
@@ -0,0 +1,67 @@
+import { defineConfig } from "islandjs";
+// import { resolve } from "path";
+
+export default defineConfig({
+  title: "CL 文档",
+  description: "编程知识点整合, 为开发者提供的编程文档",
+  icon: "/logo.png",
+  outDir: "testDist",
+  markdown: {
+    lineNumbers: true,
+  },
+  themeConfig: {
+    lastUpdatedText: "上次更新",
+    prevPageText: "上一页",
+    nextPageText: "下一页",
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2024-present Yuantu Ling",
+    },
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/cmtlyt",
+      },
+    ],
+    nav: [
+      {
+        text: "首页",
+        link: "/",
+        activeMatch: "^/$",
+      },
+      {
+        text: "文库",
+        items: [
+          {
+            text: "JavaScript",
+            link: "/javascript",
+          },
+        ],
+      },
+    ],
+    sidebar: {
+      "/": [
+        {
+          text: "介绍",
+          items: [
+            {
+              text: "快速开始",
+              link: "/article",
+            },
+          ],
+        },
+      ],
+      "/javascript/": [
+        {
+          text: "JavaScript",
+          items: [
+            {
+              text: "快速开始",
+              link: "/javascript",
+            },
+          ],
+        },
+      ],
+    },
+  },
+});
---
+++ edit-docs/index.md
@@ -0,0 +1,28 @@
+---
+pageType: home
+
+hero:
+  name: CL 文档
+  text: 编程知识点整合
+  tagline: 为开发者提供的编程文档
+  image:
+    src: /logo.png
+    alt: Note
+  actions:
+    - theme: brand
+      text: 点击查看
+      link: /article
+    - theme: alt
+      text: GitHub
+      link: https://github.com/sanyuan0704/island.js
+# features:
+#   - title: Feature 1
+#     details: Feature 1 的详细内容
+#     icon: 🪐
+#   - title: Feature 2
+#     details: Feature 2 的详细内容
+#     icon: 🧑🏻‍💻
+#   - title: Feature 3
+#     details: Feature 3 的详细内容
+#     icon: 🏃‍♂️
+---
